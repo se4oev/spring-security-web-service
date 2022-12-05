5899a6f67b9219c7be1632c375550af08122f4e3
karpenko <karpenko@reshenie-soft.ru>
2022-12-05 12:40:45 +0300

    Initial commit

---
+++ src/main/java/ru/se4oev/springsecuritywebservice/SpringSecurityWebServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.se4oev.springsecuritywebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/se4oev/springsecuritywebservice/SpringSecurityWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.se4oev.springsecuritywebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
